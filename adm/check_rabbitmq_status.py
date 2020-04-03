ee11a62785421d8df124b86c3f4cbb5254893a23
thebaptiste <jean-baptiste.veslin@meteo.fr>
2020-04-03 16:51:44 +0000

    build: improve mfbus.status

---
+++ adm/check_rabbitmq_status.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+from time import sleep
+import pika
+import os
+
+max_tries = 10
+nb_tries = 0
+port = int(os.getenv('MFBUS_RABBITMQ_AMQP_PORT'))
+
+while nb_tries <= max_tries:
+    try:
+        connection = pika.BlockingConnection(
+            pika.ConnectionParameters('localhost', port))
+        channel = connection.channel()
+        print("RabbitMQ Connection ok")
+        exit(0)
+    except Exception as e:
+        print("RabbitMQ Connection Error, try %d, sleeping 1s" % nb_tries)
+        nb_tries += 1
+        if nb_tries == max_tries:
+            print("RabbitMQ Connection Fails after 10s")
+            exit(1)
+        sleep(1)
