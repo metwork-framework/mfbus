aed7a8d831326ed4656f201eb783177f49b28258
metworkbot <metworkbot@metwork-framework.org>
2022-05-31 14:01:58 +0000

    build: sync common files from github_organization_management repository

--- .github/workflows/integration_tests.sh
+++ .github/workflows/integration_tests.sh
@@ -12,7 +12,6 @@ echo -e "baseurl=http://metwork-framework.org/pub/metwork/continuous_integration
 echo -e "gpgcheck=0\n\enabled=1\n\metadata_expire=0\n" >>/etc/yum.repos.d/metwork.repo
 
 
-
     yum -y localinstall ./rpms/metwork-mfbus*.rpm
     yum -y install make
     su --command="mfbus.init" - mfbus
@@ -20,5 +19,4 @@ echo -e "gpgcheck=0\n\enabled=1\n\metadata_expire=0\n" >>/etc/yum.repos.d/metwor
     su --command="mfbus.status" - mfbus
     if test -d "integration_tests"; then chown -R mfbus integration_tests; cd integration_tests; su --command="cd `pwd`; ./run_integration_tests.sh" - mfbus; cd ..; fi
     su --command="mfbus.stop" - mfbus
-
 
