ab415ffc529854c63b7750e59694f0d67bff129c
thebaptiste <jean-baptiste.veslin@meteo.fr>
2022-10-20 16:14:42 +0000

    build: test...

--- .github/workflows/set_variables.sh
+++ .github/workflows/set_variables.sh
@@ -101,7 +101,8 @@ fi
     
 
 
-echo "branch=${B}" >> ${GITHUB_OUTPUT}
+echo "branch=null" >> ${GITHUB_OUTPUT}
+#echo "branch=${B}" >> ${GITHUB_OUTPUT}
 echo "os=${OS_VERSION}" >> ${GITHUB_OUTPUT}
 echo "tag=${TAG}" >> ${GITHUB_OUTPUT}
 echo "dep_branch=${DEP_BRANCH}" >> ${GITHUB_OUTPUT}
